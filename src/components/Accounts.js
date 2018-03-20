e848e9bb92a6e4d9330cad261db309eec88f7307
Patrick O'Dacre <patrick@patrickwho.me>
2018-03-20 12:49:13 -0400

    Add Accounts List
    - at this point we're listing, creating and deleting
    accounts. I have yet to add the ability to update.

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -2,6 +2,13 @@ import React from 'react'
 import api from '../services/api'
 import AccountList from './partials/AccountList'
 
+/* 
+    Accounts Component
+    
+    Responsible for rendering the following:
+    - create account form
+    - accounts list
+*/
 export default class extends React.Component {
     constructor() {
         super()
@@ -11,15 +18,101 @@ export default class extends React.Component {
             description: ''
         }
     }
-    handleChange = evt => {
-        const { value, name } = evt.target
 
-        this.setState({
-            [name]: value
-        })
+    render() {
+        return (
+            <section className="fudget accounts container">
+                <div className="row">
+                    <div className="col">
+                        <form>
+                            <div className="form-row">
+                                <div className="col-md-4">
+                                    <div className="input-group mb-3">
+                                        <div className="input-group-prepend">
+                                            <label
+                                                className="input-group-text"
+                                                htmlFor="account_name"
+                                            >
+                                                Name
+                                            </label>
+                                        </div>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="account_name"
+                                            name="name"
+                                            onChange={this.handleChange}
+                                            value={this.state.name}
+                                            placeholder="Enter account name."
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="input-group mb-3">
+                                        <div className="input-group-prepend">
+                                            <label
+                                                className="input-group-text"
+                                                htmlFor="account_description"
+                                            >
+                                                Description
+                                            </label>
+                                        </div>
+
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            id="account_description"
+                                            name="description"
+                                            onChange={this.handleChange}
+                                            value={this.state.description}
+                                            placeholder="Enter account description. (optional)"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="input-group submit-right">
+                                        <button
+                                            type="button"
+                                            onClick={this.createNewAccount}
+                                            className="btn btn-primary"
+                                        >
+                                            Save
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <AccountList
+                            accounts={this.state.accounts}
+                            onDeleteClicked={this.deleteAccount}
+                        />
+                    </div>
+                </div>
+            </section>
+        )
     }
 
-    handleNewAccount = () => {
+    /* -----------------------------
+    Life Cycle Hooks
+    ------------------------------- */
+
+    componentDidMount() {
+        api()
+            .get('accounts')
+            .then(data => {
+                this.setState({ accounts: data })
+            })
+    }
+
+    /* -----------------------------
+    CRUD Operations
+    ------------------------------- */
+    createNewAccount = () => {
         const { name, description } = this.state
 
         api()
@@ -30,7 +123,13 @@ export default class extends React.Component {
             })
     }
 
-    handleDelete = id => {
+    /**
+     * Delete an account
+     *
+     * @param {number} id Account id
+     * @returns undefined
+     */
+    deleteAccount = id => {
         api()
             .del('accounts/' + id)
             .then(res => {
@@ -42,55 +141,23 @@ export default class extends React.Component {
             })
     }
 
-    componentDidMount() {
-        api()
-            .get('accounts')
-            .then(data => {
-                this.setState({ accounts: data })
-            })
-    }
+    /* -----------------------------
+    Event Handlers
+    ------------------------------- */
 
-    render() {
-        return (
-            <section className="accounts">
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="account_name">Name</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="account_name"
-                            name="name"
-                            onChange={this.handleChange}
-                            value={this.state.name}
-                            placeholder="Enter account name."
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="account_description">Description</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="account_description"
-                            name="description"
-                            onChange={this.handleChange}
-                            value={this.state.description}
-                            placeholder="Enter account description. (optional)"
-                        />
-                    </div>
-                    <button
-                        type="button"
-                        onClick={this.handleNewAccount}
-                        className="btn btn-primary"
-                    >
-                        Submit
-                    </button>
-                </form>
-                <AccountList
-                    accounts={this.state.accounts}
-                    onDeleteClicked={this.handleDelete}
-                />
-            </section>
-        )
+    /**
+     * Track the form input values
+     *
+     * name, description
+     *
+     * @param {object} evt change event
+     * @returns undefined
+     */
+    handleChange = evt => {
+        const { value, name } = evt.target
+
+        this.setState({
+            [name]: value
+        })
     }
 }
--- src/components/partials/AccountList.js
+++ src/components/partials/AccountList.js
@@ -1,28 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled, { injectGlobal } from 'styled-components'
+import ListGroupItem from './ListGroupItem'
+import moment from 'moment'
 
 export default props => {
     function handleClick(evt) {
+        // call the fn passed into props as onDeleteClicked
         props.onDeleteClicked(parseInt(evt.target.attributes.acc.value))
     }
     return (
-        <ul className="list-group">
+        <ul className="list-group fudget">
             {props.accounts.map(acc => (
-                <li className="list-group-item" key={acc.id}>
-                    <span className="info">
-                        {acc.name} - {acc.description} (created:{' '}
-                        {acc.created_at})
-                    </span>
-                    <span className="del">
+                <ListGroupItem key={acc.id}>
+                    <Link
+                        to={`/accounts/${acc.id}`}
+                        className="fudget-router-link"
+                    >
+                        <div className="info">
+                            {acc.name} - {acc.description} ( Created:{' '}
+                            {moment(acc.created_at).format('LL')})
+                        </div>
+                    </Link>
+                    <div className="del">
                         <button
+                            className="btn btn-secondary"
                             type="button"
                             onClick={handleClick}
                             acc={acc.id}
                         >
                             Delete
                         </button>
-                    </span>
-                </li>
+                    </div>
+                </ListGroupItem>
             ))}
         </ul>
     )
 }
+
+/* Styles */
+
+injectGlobal`
+    .list-group-item .fudget-router-link {
+        text-align: left;
+        flex: 1;
+    }
+`
