a475b847feacdca7c2ae27222ae4f3906ffd2206
Patrick O'Dacre <patrick@patrickwho.me>
2018-03-20 13:55:34 -0400

    Update styles to use SCSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
-// import './App.css'
+import './App.css'
 import './assets/css/jumbotron.css'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
